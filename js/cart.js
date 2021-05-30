0eaffcfa61d52d653f53546dde0f55a35ee35fc4
Your Name <you@example.com>
2021-05-30 15:30:15 +0200

    update

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,9 @@
 const cartItems = JSON.parse(localStorage.getItem("cartList"));
 const cartContainer = document.querySelector(".cart-list");
 const totalContainer = document.querySelector(".total");
+const checkoutButton = document
+  .querySelector(".checkout")
+  .addEventListener("click", deleteCartList);
 
 let total = 0;
 cartItems.forEach(function (cartElement) {
@@ -13,3 +16,8 @@ cartItems.forEach(function (cartElement) {
 `;
 });
 totalContainer.innerHTML = `Total: $${total.toFixed(2)}`;
+
+function deleteCartList() {
+  window.localStorage.removeItem("cartList");
+  window.location.replace("complete.html");
+}
